400c1a56ac46a07cea30dc1f095b4048d51f5eff
Eliyanto Sarage <sayaptawon.rtnwsb@gmail.com>
2024-08-09 03:21:29 +0700

    Update firebaseConfig.js

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -3,14 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue, update } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAQqCb4obTgFMozVOg3JqJJ-ZCmdPhITLs',
-  authDomain: 'rutan-wonosobo-0.firebaseapp.com',
-  databaseURL: 'https://rutan-wonosobo-0-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'rutan-wonosobo-0',
-  storageBucket: 'rutan-wonosobo-0.appspot.com',
-  messagingSenderId: '587999781707',
-  appId: '1:587999781707:web:7fb68389a77364bb9a7ece',
-  measurementId: 'G-2ZW152V3VP',
+  apiKey: 'AIzaSyANl8iBz7vGR3sDJMawtKHNfYiq4cNISuM',
+  authDomain: 'sayaptawon-5d681.firebaseapp.com',
+  databaseURL: 'https://sayaptawon-5d681-default-rtdb.firebaseio.com',
+  projectId: 'sayaptawon-5d681',
+  storageBucket: 'sayaptawon-5d681.appspot.com',
+  messagingSenderId: '1015086538206',
+  appId: '1:1015086538206:web:859768cad5e15e99b6114f',
+  measurementId: 'G-EKL02XW5HK',
 };
 
 // Inisialisasi Firebase
