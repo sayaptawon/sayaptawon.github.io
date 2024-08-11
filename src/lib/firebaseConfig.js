4016b25c57b9c8f942f3f793dd84410a4c50d1d3
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-11 11:09:08 +0700

    Fix data and handling in firebaseConfig.js

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,21 +1,17 @@
-// src/lib/firebaseConfig.js
-
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue, update } from 'firebase/database';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANl8iBz7vGR3sDJMawtKHNfYiq4cNISuM",
-  authDomain: "sayaptawon-5d681.firebaseapp.com",
-  databaseURL: "https://sayaptawon-5d681-default-rtdb.firebaseio.com",
-  projectId: "sayaptawon-5d681",
-  storageBucket: "sayaptawon-5d681.appspot.com",
-  messagingSenderId: "1015086538206",
-  appId: "1:1015086538206:web:859768cad5e15e99b6114f",
-  measurementId: "G-EKL02XW5HK"
+  apiKey: 'AIzaSyANl8iBz7vGR3sDJMawtKHNfYiq4cNISuM',
+  authDomain: 'sayaptawon-5d681.firebaseapp.com',
+  databaseURL: 'https://sayaptawon-5d681-default-rtdb.firebaseio.com',
+  projectId: 'sayaptawon-5d681',
+  storageBucket: 'sayaptawon-5d681.appspot.com',
+  messagingSenderId: '1015086538206',
+  appId: '1:1015086538206:web:2fd93cc96beaff47b6114f',
+  measurementId: 'G-5SFGGCZ55R',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
-export { database, ref, onValue, update };
+export const database = getDatabase(app);
