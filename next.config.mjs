2aa58685154b1f89efd2a0eb221206996c8fe382
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-11 10:57:44 +0700

    Change productionBrowserSourceMaps from true to false

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
   },
   reactStrictMode: true,
   output: 'standalone',
-  productionBrowserSourceMaps: true,
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
