9a87059331eb7ba3c3d910356e6f7b310b7664c8
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-14 05:10:51 +0700

    Update firebaseConfig.js

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyANl8iBz7vGR3sDJMawtKHNfYiq4cNISuM',
@@ -15,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const database = getDatabase(app);
+export const firestore = getFirestore(app);
