688e26ada25d5d51161b3c5012929a4ebe1e0bf7
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-14 08:17:31 +0700

    Optimize: Improve Largest Contentful Paint (LCP)
    - Added placeholder blur image for faster loading of team photos.
    - Refactored component to ensure better image loading performance.
    - Enhanced layout for consistent rendering and improved user experience.

--- src/components/Team.js
+++ src/components/Team.js
@@ -8,11 +8,7 @@ const Team = () => {
   const [isPageLoaded, setIsPageLoaded] = useState(false);
 
   useEffect(() => {
-    const handlePageLoad = () => {
-      if (document.readyState === 'complete') {
-        setIsPageLoaded(true);
-      }
-    };
+    const handlePageLoad = () => setIsPageLoaded(true);
 
     if (document.readyState === 'complete') {
       setIsPageLoaded(true);
@@ -23,17 +19,10 @@ const Team = () => {
     return () => window.removeEventListener('load', handlePageLoad);
   }, []);
 
-  const imageStyle = {
-    width: '96px',
-    height: '96px',
-    objectFit: 'cover',
-    objectPosition: 'top',
-  };
-
   return (
     <section data-theme='corporate' className='p-4 border-b border-gray-200 mb-4'>
       <div className='text-center mb-8'>
-        <h2 className='text-xl md:text-2xl lg:text-3xl font-semibold text-primary mb-4 text-center'>Our Team</h2>
+        <h2 className='text-xl md:text-2xl lg:text-3xl font-semibold text-primary mb-4'>Our Team</h2>
         <p className='text-lg text-base-content'>Ketahui lebih lanjut tentang orang-orang di balik proyek ini</p>
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6'>
@@ -43,13 +32,13 @@ const Team = () => {
               {!isPageLoaded && <div className='loading loading-spinner loading-lg bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400'></div>}
               {isPageLoaded && (
                 <div className='mask mask-circle w-24 h-24 flex items-start justify-center'>
-                  <Image src={member.photo} alt={member.name} width={96} height={96} style={imageStyle} loading='lazy' />
+                  <Image src={member.photo} alt={member.name} width={96} height={96} style={{ width: '96px', height: '96px', objectFit: 'cover', objectPosition: 'top' }} loading='lazy' placeholder='blur' blurDataURL='https://fastly.picsum.photos/id/278/20/20.jpg?blur=10&hmac=mIMB2jGMPxpBwZbfY64XdKqrhu9AL0cSmKMFci1Rl9w' />
                 </div>
               )}
             </figure>
-            <div className='card-body bg-base-200 justify-center items-center text-center p-4'>
+            <div className='card-body bg-base-200 text-center p-4'>
               <h3 className='text-xl font-semibold mb-2'>{member.name}</h3>
-              <div className='badge badge-primary text-white rounded-full mb-2'>{member.role}</div>
+              <div className='badge badge-primary text-white rounded-full mb-2 mx-auto'>{member.role}</div>
               <p className='text-base'>{member.description}</p>
             </div>
           </div>
