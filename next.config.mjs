0c410de00faa1c983aa6b1c39b4d77f9ed1d4f99
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-18 21:30:09 +0700

    fix: refine nextConfig settings
    - Added ESLint setting to ignore during builds.
    - Updated output setting to 'standalone'.
    - Verified and cleaned up image remote patterns.
    
    Co-Authored-By: Jagad Brahma Wiraatmaja <officialelsa21@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,19 @@ const nextConfig = {
         hostname: 'cdn.jsdelivr.net',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dl.snapcdn.app',
+        pathname: '/**',
+      },
     ],
   },
   reactStrictMode: true,
   output: 'standalone',
   productionBrowserSourceMaps: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
