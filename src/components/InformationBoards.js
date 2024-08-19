8dac2c9c62523cd2c0d510f43c9a3acc7d22c7fe
Sayap Tawon <sayaptawon.rtnwsb@gmail.com>
2024-08-20 00:26:52 +0700

    Update InformationBoards.js

--- src/components/InformationBoards.js
+++ src/components/InformationBoards.js
@@ -3,24 +3,24 @@ import data from '@/data/informationBoardsData.json';
 
 const InformationBoards = () => {
   return (
-    <section data-theme='corporate' className='p-4 border-b border-gray-200'>
-      <div className='grid gap-6 md:grid-cols-2 mb-4'>
+    <section data-theme='corporate' className='p-6 border-b border-gray-200'>
+      <div className='grid gap-2 md:grid-cols-2'>
         {/* Tabel Jam Layanan */}
-        <div className='card bg-white shadow-md rounded-lg p-4'>
+        <div className='bg-white shadow-lg rounded-lg'>
           <h3 className='text-lg md:text-xl font-semibold text-primary mb-4 text-center'>
             {data.informationBoards[0].title}
           </h3>
           <div className='overflow-x-auto'>
-            <table className='table w-full table-fixed'>
+            <table className='table w-full table-auto'>
               <thead className='bg-primary text-white'>
                 <tr>
-                  <th className='py-3 px-4'>Hari</th>
-                  <th className='py-3 px-4'>Jam</th>
+                  <th className='py-3 px-4 text-left'>Hari</th>
+                  <th className='py-3 px-4 text-left'>Jam</th>
                 </tr>
               </thead>
               <tbody>
                 {data.informationBoards[0].data.map((item, index) => (
-                  <tr key={index} className='hover:bg-gray-100'>
+                  <tr key={index} className='hover:bg-gray-50'>
                     <td className='py-3 px-4 font-medium text-gray-700'>
                       {item.day}
                     </td>
@@ -38,21 +38,21 @@ const InformationBoards = () => {
           </div>
         </div>
         {/* Tabel Hari Kunjungan */}
-        <div className='card bg-white shadow-md rounded-lg p-4'>
+        <div className='bg-white shadow-lg rounded-lg'>
           <h3 className='text-lg md:text-xl font-semibold text-primary mb-4 text-center'>
             {data.informationBoards[1].title}
           </h3>
           <div className='overflow-x-auto'>
-            <table className='table w-full table-fixed'>
+            <table className='table w-full table-auto'>
               <thead className='bg-primary text-white'>
                 <tr>
-                  <th className='py-3 px-4'>Hari</th>
-                  <th className='py-3 px-4'>Keterangan</th>
+                  <th className='py-3 px-4 text-left'>Hari</th>
+                  <th className='py-3 px-4 text-left'>Keterangan</th>
                 </tr>
               </thead>
               <tbody>
                 {data.informationBoards[1].data.map((item, index) => (
-                  <tr key={index} className='hover:bg-gray-100'>
+                  <tr key={index} className='hover:bg-gray-50'>
                     <td className='py-3 px-4 font-medium text-gray-700'>
                       {item.day}
                     </td>
